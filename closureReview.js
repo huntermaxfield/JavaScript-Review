a2c4b985e2c75c102f7d2ae9739043dbc9550652
huntermaxfield <huntermaxfield1@gmail.com>
2015-07-11 14:18:06 -0600

    done

--- closureReview.js
+++ closureReview.js
@@ -10,13 +10,36 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //code here
-
+var callJake = callFriend();
+console.log(callJake('4352159248'));
 
 
 /*
 
 Write a function that accepts a function as it's first argument and returns a new function (which calls the original function that was passed in) that can only ever be executed once.
 
+
 Once completed, add a second arguments that allows the function to be executed N number of times. After the function has been called N number of times, console.log('STAHHP');
 
+*/
+
+function one(function two(){ }, num) {
+  counter = 0;
+  if(counter < num) {
+    //something
+  } else {
+    console.log('stahhhhp');
+  }
+}
+
+/*
+var executed = false;
+
+return function three() {
+  if(!excuted) {
+    two();
+    executed = true;
+  }
+
+}
 */
--- objectReview.js
+++ objectReview.js
@@ -2,19 +2,35 @@
 
   //Your code here
 
+var favoriteThings = {
+    band: 'blink 182',
+    food: 'sushi',
+    person: 'sierra',
+    book: 'freakonomics',
+    movie: 'that one movie',
+    holiday: 'christmas'
+}
+
+
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
   //Your code here
+
+favoriteThings.car = 'saab';
+favoriteThings.brand = 'new balance';
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
   //Your code here
 
+favoriteThings.food = 'Lettuce';
+
 //Now, alert your favorite person, then alert your favorite book.
 
   //Your code here
 
-
+console.log(favoriteThings.person);
+console.log(favoriteThings.book);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -33,15 +49,26 @@ var user = {
 
   //Your code here
 
+for(key in user) {
+    if(!user[key]) {
+        delete user[key];
+    }
+}
+
+
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
   //Your code here
+
+user.name = 'hunter maxfield';
+user.pwHash = 'i dunno';
+user.username = 'hmaxfield';
 
 //Now console.log your object and make sure it looks right.
 
   //Your code here
 
-
+console.log(user);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -51,15 +78,24 @@ var user = {
 
   //Your code here
 
+var methodCollection = {};
+
 //Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
   //Your code here
 
+methodCollection.alertHello = function() {
+    console.log('hello');
+}
+
+methodCollection.logHello = function() {
+    console.log('hello');
+}
 //Now call your alertHello and logHello methods. 
 
   //Your code here
 
-
+methodCollection.alertHello();
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -68,5 +104,23 @@ var user = {
 //write a function called voweler that accepts a string, and returns an object with the keys being all the vowels in that string, and the values being how many times that particular vowel was in the string.
 //voweler("This is a test") --> {i: 2, a: 1, e: 1};
 
+function voweler(str) {
+    var counter = 0;
+    vowels = ['a','e','i','o','u'];
+    newObject = {
 
+    };
+    for(var i = 0; i < vowels.length; i++) {
+        for(var j = 0; j < str.length; j++) {
+            if(str[j] === vowels[i]) {
+                counter++;
+            }
+        }
+        console.log(vowels[i], counter)
+        newObject[vowels[i]] = counter;
+        counter = 0;
+    }
+    return newObject;
+}
 
+voweler("This is a test");
